4d75fd8841ba542feeeccfdf3a10db39f6e7d9b0
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2023-12-11 23:20:45 +0530

    Updating the bash script

--- generate-results.sh
+++ generate-results.sh
@@ -5,6 +5,7 @@ g++ Exchange.cpp -o Exchange
 
 # Run your program with generated input
 for input_file in inputs/*.csv; do
+    echo "Generating the execution report for order - $input_file"
     ./Exchange <<EOF
 $input_file
 EOF

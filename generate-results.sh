d804df02f98bfbd13e983592f11ef70ffa7947eb
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2023-12-13 17:05:41 +0530

    Update the driver bash script

--- generate-results.sh
+++ generate-results.sh
@@ -6,7 +6,12 @@ g++ Exchange.cpp -o Exchange
 # Run your program with generated input
 for input_file in inputs/*.csv; do
     echo "Generating the execution report for order - $input_file"
+    start_time=$(date +%s.%N)
     ./Exchange <<EOF
 $input_file
 EOF
+    end_time=$(date +%s.%N)
+    execution_time=$(echo "$end_time - $start_time" | bc)
+    echo "Execution time for $input_file: $execution_time seconds"
+    echo "-------------------------------------------------------"
 done
